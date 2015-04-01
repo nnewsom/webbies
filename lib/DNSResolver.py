215cf2fb41e5c993ff62b39a459158310380a4e5
Nick Newsom <nicholasnewsom824@gmail.com>
2015-04-01 13:27:01 -0700

    Try catch for failed DNS lookups

--- lib/DNSResolver.py
+++ lib/DNSResolver.py
@@ -15,14 +15,20 @@ class DNSResolver(object):
     @asyncio.coroutine
     def query_name(self,hostname):
         if hostname not in self.lookup_history:
-            ips = yield from self.resolver.query(hostname,'A')
+            try:
+                ips = yield from self.resolver.query(hostname,'A')
+            except aiodns.error.DNSError:
+                ips = []
             self.lookup_history[hostname] = ips
         return self.lookup_history[hostname]
 
     @asyncio.coroutine
     def query_ip(self,ip):
         if ip not in self.rlookup_history:
-            hostnames = yield from self.resolver.query(pycares.reverse_address(ip),"PTR")
+            try:
+                hostnames = yield from self.resolver.query(pycares.reverse_address(ip),"PTR")
+            except aiodns.error.DNSError:
+                hostnames = []
             self.rlookup_history[ip] = hostnames
         return self.rlookup_history[ip]
 
